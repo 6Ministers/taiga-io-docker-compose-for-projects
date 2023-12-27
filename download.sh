fb94f5f5fb5af11385a0c6bbf9a540c0d44c8c43
6ministers <manyatovskiy@gmail.com>
2023-12-27 18:00:53 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 DESTINATION=$1
 
 # clone Flectra directory
-git clone --depth=1 https://github.com/6Ministers/taiga-io-kanban-for-business-project $DESTINATION
+git clone --depth=1 https://github.com/6Ministers/taiga-io-docker-compose-for-projects $DESTINATION
 rm -rf $DESTINATION/.git
 
 
